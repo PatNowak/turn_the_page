e0e0de8894bb323b837cebd98cd4178de31ec0b6
Patryk Nowak <nowakp01@stepstone.com>
2017-08-03 08:20:08 +0200

    added handling the modules

--- lib/turn_the_page.ex
+++ lib/turn_the_page.ex
@@ -11,7 +11,7 @@ defmodule TurnThePage do
     and Stream / Enum functions for list.
 
     Parameters:
-      - query or list
+      - name of the module, query or list
       - keyword list in format [page: page, limit: limit]
   """
   def paginate(_collection, [page: page, limit: _limit]) when page <= 0 do
@@ -20,6 +20,10 @@ defmodule TurnThePage do
 
   def paginate(_collection, [page: _page, limit: limit]) when limit <= 0 do
     raise ArgumentError, message: "Limit should be positive integer"
+  end
+
+  def paginate(module_name, [page: page, limit: limit]) when is_atom(module_name) do
+    paginate(from m in module_name, [page: page, limit: limit])
   end
 
   def paginate(%Ecto.Query{} = query, [page: page, limit: limit]) do
